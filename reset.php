905f31ebfa23c33eeb3c403b0ee99ce5c6432e3a
andi <andi.bardhi@fshnstudent.info>
2020-04-28 22:50:22 +0200

    Ndryshimi i stilit ne faqen reset

--- reset.php
+++ reset.php
@@ -1,32 +1,36 @@
-<?php require_once('includes/header.php')?>
-<?php require_once('includes/navbar.php')?>
-
+<?php include_once('functions/config.php'); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>َPet&Us Login</title>
+    <link rel="stylesheet" href="css/style-login-register.css">
+    
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  </head>
+    
+    <br>
+    <br>
     <div class="container">
-        <div class="row">
-            <div class="col-8 col-lg-6 m-auto">
-                <div class="card bg-light mt-5 py-2">
-                    <div class="card-title">
-                        <h2 class="text-center">Vendosni fjalëkalimin e ri</h2>
-                    </div>
-                    <?php 
-                        reset_password();
-                        display_message();
-                    ?>
-
-                    <div class="card-body">
-                        <form method="POST">
-                            <input type="password" name="reset-password" placeholder="Fjalëkalim" class="form-control py-2 mb-2">
-                            <input type="password" name="creset-password" placeholder="Konfirmo Fjalëkalim" class="form-control py-2 mb-2">
-                            <input type="hidden" name="token" value="<?php token_generator(); ?>">
-                    </div>
-                    <div class="card-footer">
-                            <button class="btn btn-danger float-left">Anullo</button>
-                            <button class="btn btn-success float-right">Dërgo fjalëkalim</button>
-                        </form>
-                    </div>
-                </div>
+        <?php 
+                reset_password();
+                display_message();
+        ?>
+        <div class="row align-items-center justify-content-center h-100">
+            <div class="col-6 mx-auto">
+                <form class="box" method="POST">
+                    <h1>Vendosni fjalëkalimin e ri</h1>
+                    <input type="password" name="reset-password" id="username" placeholder="Fjalëkalim">
+                    <input type="password" name="creset-password" id="password" placeholder="Konfirmo Fjalëkalim">
+                    <input type="hidden" name="token" value="<?php token_generator(); ?>">
+                    <button id="btn btn-success float-right"> Dërgo </button> 
+                </form>
             </div>
         </div>
     </div>
 
 <?php require_once('includes/footer.php')?>
+
+</body>
+</html>
