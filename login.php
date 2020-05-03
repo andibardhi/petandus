616d7f16bfdb3a14ebf355a00f09cd786bf91aae
andi <andi.bardhi@fshnstudent.info>
2020-05-03 20:11:41 +0200

    Shtim i reveal password tek faqja login dhe ndryshim i css tek faqja homepage

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>َPet&Us Login</title>
     <link rel="stylesheet" href="css/style-login-register.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   </head>
@@ -15,16 +15,15 @@
                 display_message();
                 login_validation();
         ?>
-        <div class="alert alert-danger text-center" id="error" style="visibility: hidden;">
-        Username ose fjalëkalim i gabuar.
-        
-        </div>
+        <div class="alert alert-danger text-center" id="error" style="visibility: hidden;"></div>
         <div class="row align-items-center justify-content-center h-100">
         <div class="col-6 mx-auto">
         <div class="box" >
             <h1>Login</h1>
             <input type="text" name="username" id="username" placeholder="Username">
-            <input type="password" name="password" id="password" placeholder="Fjalëkalim">
+            <div class="reveal-password">
+                <input type="password" name="password" id="password" placeholder="Fjalëkalim"><i style="font-size:24px;cursor: pointer;" onclick="togglePassword()" class="fa">&#xf06e;</i>
+            </div>
             <input type="checkbox" name="remember" id="remember"> <label for="remember">Më mbaj mend</label><br>
             <a href="./recover.php">Keni harruar fjalëkalimin?</a> 
             <button id="btn"> LOGIN </button> 
@@ -47,15 +46,26 @@
                         }
                         else{
                             $('#error').css("visibility", "visible");
+                            $('#error').html("Username ose fjalëkalim i gabuar.");
                         }
                     }
                     );
                 }
                 else{
-                    alert('Ploteso fushat bosh');
+                    $('#error').css("visibility", "visible");
+                    $('#error').html("Ploteso fushat bosh.");
                 }
             });
         });
+
+        function togglePassword(){
+            if($("#password").attr("type")=='password'){
+                $("#password").attr("type","text");
+            }
+            else{
+                $("#password").attr("type","password");
+            }
+        }
     </script>
 
 <?php require_once('includes/footer.php')?>
