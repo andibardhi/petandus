461ac0bdb96425b2673453bed50b3240b5dde9ef
exozero7 <foti.kerkeshi@hotmail.com>
2020-05-12 17:59:36 +0200

    update posts.php

--- posts.php
+++ posts.php
@@ -1,29 +1,24 @@
+<?php include_once('functions/config.php'); ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Posts Pets&Us </title>
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta charset="utf-8">
-      <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-      <link rel = "stylesheet" type = "text/css" href = "./css/header-style.css">
-      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-      <link rel = "stylesheet" type = "text/css" href = "./css/homepage-style.css">
-      <link rel = "stylesheet" type = "text/css" href = "./css/posts-style.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title> Posts Pet&Us </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel = "stylesheet" type = "text/css" href = "./css/header-style.css">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <link rel = "stylesheet" type = "text/css" href = "./css/homepage-style.css">
+        <link rel = "stylesheet" type = "text/css" href = "./css/posts-style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     
     <body style="background-color: #fff9e9;">
-        <?php
-            include_once('./includes/navbar.php');
-        ?>
-
-
-
-        <br> <br>
-
+        <?php include_once('./includes/navbar.php'); ?>
+        <br><br>
         <!-- filer start -->
         <div class="container col-md-8" style="padding: 0px;">
             <form method="get"> 
@@ -56,7 +51,7 @@
                     <option value="veteriner">veteriner</option>
                 </select>
             </div>
-            <button type="submit    " id="btncolor" class="btn" style="margin-left: 2%; background-color:#e45225; border-radius: 40px; color:white;">Shto</button>
+            <button type="submit" id="btncolor" class="btn" style="margin-left: 2%; background-color:#e45225; border-radius: 40px; color:white;">Shto</button>
         </form>
         </div> <!-- filter end -->
         
@@ -77,7 +72,7 @@
                         </div>
 
                         <div class="col-5">
-                                <img src="./img/profile-dog.jpg">
+                            <img alt="post_photo">
                         </div>
                     </div>
                 </div>
@@ -93,7 +88,7 @@
                             unknown printer took a galley of type ...
                         </div>
                         <div class="col-5">
-                            <img src="./img/profile-cat.jpg">
+                            <img alt="post_photo">
                         </div>
                     </div>
                 </div>
@@ -109,12 +104,12 @@
                             unknown printer took a galley of type ...
                         </div>
                         <div class="col-5">
-                            <img src="./img/profile-fish.jpg">
+                            <img >
                         </div>
                     </div>
                 </div>
             </a>
-            <a href="./single-post.php"> 
+            <a href="./single-post.php">    
                 <div class="row single-post">
                     <title class="row">Titull</title>
                     <div class="row justify-content-around">
@@ -126,8 +121,11 @@
                         </div>
 
                         <div class="col-5">
-                                <img src="./img/profile-dog.jpg">
+                            <img alt="post_photo">
                         </div>
+                    </div>
+                    <div class="row">
+                    05/09/2020  -    @fotikerkeshi  -   Adoptim   -   #2432489
                     </div>
                 </div>
             </a>
@@ -142,7 +140,7 @@
                             unknown printer took a galley of type ...
                         </div>
                         <div class="col-5">
-                            <img src="./img/profile-fish.jpg">
+                            <img alt="post_photo">
                         </div>
                     </div>
                 </div>
@@ -156,5 +154,31 @@
             include_once('./includes/footer.php');
         ?>
     </body>
+
+    <script>
+
+        var data = <?php echo json_encode(retrieve_data()); ?>;
+        var imgs = <?php echo json_encode(show_photo()); ?>;
+
+        console.log(data);
+        var oldSrc = 'data:image/jpeg;base64,';
+
+        var images = $('img');
+
+        $.each(images, function(i, v){
+            $(this).attr('src', oldSrc + imgs[i]);
+        });
+
+        $(document).ready(function () {
+            $("a").click(function () {
+                
+
+
+            });
+        });
+            
+        
+    </script>
+
 </html>
 
