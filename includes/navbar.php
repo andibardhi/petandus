3f4aaa150b53198d50d4b766473022e8f8ded2ae
Aldona Lala <aldona.lala@fshnstudent.info>
2020-05-15 18:18:36 +0200

    shtimi i butonit kryefaqe tek navbari

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,22 +1,24 @@
 <header> 
-            <div class="header">
-                <a href="./index.php" class="logo"> PET&US </a>
-                <div class="header-right">
-                    <a class="active" href="./posts.php">Postime</a>
-                    <a href="./blog.php">Blog</a>
-                    <?php 
-                        if(isset($_SESSION['username']) OR isset($_COOKIE['username'])){
-                    ?>
-                    <a href="./profile.php" id="logreg"> Profili </a> 
-                    <a href="./logout.php" id="logreg">Dil </a> 
-                    <?php  
-                        }else{
-                    ?>
-                    <a href="./login.php" id="logreg">Logohu </a>
-                    <a href="./register.php" id="logreg">Regjistrohu</a>
-                    <?php
-                        }
-                    ?>
-                </div>
-            </div>
-        </header>
+    <div class="header">
+        <a href="./index.php" class="logo"> PET&US </a>
+
+        <div class="header-right">
+            <a href="./index.php">Kryefaqja</a>
+            <a class="active" href="./posts.php">Postime</a>
+            <a href="./blog.php">Blog</a>
+            <?php 
+                if(isset($_SESSION['username']) OR isset($_COOKIE['username'])){
+            ?>
+            <a href="./profile.php" id="logreg"> Profili </a> 
+            <a href="./logout.php" id="logreg">Dil </a> 
+            <?php  
+                }else{
+            ?>
+            <a href="./login.php" id="logreg">Logohu </a>
+            <a href="./register.php" id="logreg">Regjistrohu</a>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</header>
