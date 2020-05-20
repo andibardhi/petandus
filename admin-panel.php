89d8b54ec87bbe0a40c892e1f639ba411df134fa
andi <andi.bardhi@fshnstudent.info>
2020-05-20 20:31:33 +0200

    Shtim i panelit te adminit.

---
+++ admin-panel.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pet&Us - Paneli i Adminit</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/header-style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <style>
+        .btn{
+            font-size: 10px;
+            padding: 4px;
+        }        
+
+        .buttons{
+            margin-top:10px;
+        }
+
+        .single{
+            margin-top:10px;
+            border:1px solid black;
+            border-radius:5px;
+            cursor:pointer;
+            padding: 22px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+        include_once('./includes/navbar.php');
+        include_once('functions/config.php'); 
+        include_once('functions/admin.php'); 
+        $con = mysqli_connect("localhost","root","","petus");
+        if(isset($_SESSION['role'])){
+            if($_SESSION['role']=='U'){
+                header("Location: ./index.php");
+            }
+        }else{
+            header("Location: ./index.php");
+        }
+        
+    ?>
+    <div class="container">
+        <div class="row buttons">
+            <div class="col-4">
+                <div class="row justify-content-center">
+                    <div class="">Postet</div>
+                </div>
+                <?php
+                $sql = "SELECT * FROM POST";
+                $result = mysqli_query($con,$sql);
+                while($row = $result->fetch_assoc()) {
+                    
+                ?>
+                <div class="col single">
+                    <div class="row"><?php echo $row['titull'] ?></div>
+                    <div class="row"><?php echo $row['pershkrim'] ?></div>
+                    <button onclick="fshipost(<?php echo $row['id'] ?>)" class="btn btn-danger">Fshi post</button>
+                </div>
+                <?php                    
+                }   
+                ?>
+            </div>
+            <div class="col-4">
+                <div class="row justify-content-center">
+                    <div class="">Bloget</div>
+                </div>
+                <?php
+                    $sql = "SELECT * FROM BLOG";
+                    $result = mysqli_query($con,$sql);
+                    while($row = $result->fetch_assoc()) {
+                        
+                    ?>
+                    <div class="col single">
+                        <div class="row"><?php echo $row['titull'] ?></div>
+                        <div class="row"><?php echo $row['pershkrim'] ?></div>
+                        <button onclick="fshiblog(<?php echo $row['id'] ?>)" class="btn btn-danger">Fshi blog</button>
+                    </div>
+                <?php                    
+                }   
+                ?>
+            </div>
+            <div class="col-4">
+                <div class="row justify-content-center">
+                    <div class="">Perdorues</div>
+                </div>
+                <?php
+                $sql = "SELECT * FROM USER";
+                $result = mysqli_query($con,$sql);
+                while($row = $result->fetch_assoc()) {
+                    
+                ?>
+                <div class="col single">
+                    <div class="row"><?php echo $row['username'] ?></div>
+                    <div class="row"><?php echo $row['email'] ?></div>
+                    <button onclick="fshiperdorues(<?php echo $row['id'] ?>)" class="btn btn-danger">Fshi perdorues</button>
+                    <?php
+                        if($row['role']=='U'){
+                    ?>
+                    <button onclick="bejadmin(<?php echo $row['id'] ?>)" class="btn btn-success">Bej admin</button>
+                    <?php
+                        }
+                        else{
+                    ?>
+                    <button onclick="hiqadmin(<?php echo $row['id'] ?>)" class="btn btn-danger">Hiq admin</button>
+                    <?php  
+                        }
+                    ?>
+                </div>
+                <?php                    
+                }   
+                ?>
+            </div>
+        </div>
+    </div>
+    <script>
+        function fshipost(id){
+            if(confirm('Jeni i sigurte qe doni te fshini kete post?')){
+                $.post('./functions/admin.php', {id:id, action:'delete-post'}, function (response) {
+                    if(response=='success'){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                });
+            }
+        }
+        function fshiblog(id){
+            if(confirm('Jeni i sigurte qe doni te fshini kete blog?')){
+                $.post('./functions/admin.php', {id:id, action:'delete-blog'}, function (response) {
+                    if(response=='success'){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                });
+            }
+        }
+        function fshiperdorues(id){
+            if(confirm('Jeni i sigurte qe doni te fshini kete perdorues?')){
+                $.post('./functions/admin.php', {id:id, action:'delete-user'}, function (response) {
+                    if(response=='success'){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                });
+            }
+        }
+
+        function bejadmin(id){
+            if(confirm('Jeni i sigurte qe doni te beni kete perdorues admin?')){
+                $.post('./functions/admin.php', {id:id, action:'make-admin'}, function (response) {
+                    if(response=='success'){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                });
+            }
+        }
+
+        function hiqadmin(id){
+            if(confirm('Jeni i sigurte qe doni te hiqni rolin e adminit nga ky perdorues?')){
+                $.post('./functions/admin.php', {id:id, action:'remove-admin'}, function (response) {
+                    if(response=='success'){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                });
+            }
+        }
+    </script>
+</body>
+</html>
---
+++ functions/admin.php
@@ -0,0 +1,68 @@
+<?php
+    if(isset($_POST['action'])){
+
+
+        if($_POST['action']=='delete-post'){
+            $id = $_POST['id'];
+
+            $con = mysqli_connect("localhost","root","","petus");
+            $sql = "DELETE FROM POST WHERE id=$id";
+            if ($con->query($sql) === TRUE) {
+                echo "success";
+            } else {
+                echo "Ndodhi nje problem: " . $con->error;
+            }
+        }
+
+        if($_POST['action']=='delete-blog'){
+            $id = $_POST['id'];
+
+            $con = mysqli_connect("localhost","root","","petus");
+            $sql = "DELETE FROM BLOG WHERE id=$id";
+            if ($con->query($sql) === TRUE) {
+                echo "success";
+            } else {
+                echo "Ndodhi nje problem: " . $con->error;
+            }
+        }
+
+        if($_POST['action']=='delete-user'){
+            $id = $_POST['id'];
+
+            $con = mysqli_connect("localhost","root","","petus");
+            $sql1 = "DELETE FROM USER WHERE id=$id";
+            $sql2 = "DELETE FROM PROFIL WHERE userId=$id";
+            if ($con->query($sql1) === TRUE && $con->query($sql2)=== TRUE) {
+                    echo "success";      
+            } else {
+                echo "Ndodhi nje problem: " . $con->error;
+            }
+        }
+
+        if($_POST['action']=='make-admin'){
+            $id = $_POST['id'];
+
+            $con = mysqli_connect("localhost","root","","petus");
+            $sql = "UPDATE USER SET ROLE = 'A' WHERE id = $id";
+            if ($con->query($sql) === TRUE ) {
+                    echo "success";      
+            } else {
+                echo "Ndodhi nje problem: " . $con->error;
+            }
+        }
+
+        if($_POST['action']=='remove-admin'){
+            $id = $_POST['id'];
+
+            $con = mysqli_connect("localhost","root","","petus");
+            $sql = "UPDATE USER SET ROLE = 'U' WHERE id = $id";
+            if ($con->query($sql) === TRUE ) {
+                    echo "success";      
+            } else {
+                echo "Ndodhi nje problem: " . $con->error;
+            }
+        }
+
+
+    }
+?>
