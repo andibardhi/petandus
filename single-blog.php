ee611f60c6b312927b71d696dfe567491c59689a
Aldona Lala <aldona.lala@fshnstudent.info>
2020-05-20 22:23:45 +0200

    krijimi i faqes single blog

--- single-blog.php
+++ single-blog.php
@@ -19,7 +19,23 @@
     <?php include_once('./includes/navbar.php');?>
     <br>
 
-    <?php build_single_post($_GET['id']) ?>;
+    <div class='container'>
+            
+            <div class='row justify-content-center title'>
+                <span> Titulli </span>
+            </div>
+            <div class='row description'>
+                <span> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."</span>
+            </div>
+            <div class='row justify-content-center'>
+                <img src="./img/b.jpg" alt='post_photo'>
+            </div>
+            <div class='row categories'>
+                <div class='col-4 category'> Kategoria </div>
+                <div class='col-4 city'> Qyteti </div>
+                <div class='col-4 animal'> Kafsha </div>
+            </div>
+        </div>
 
     <?php include_once('./includes/footer.php'); ?>
 </body>
